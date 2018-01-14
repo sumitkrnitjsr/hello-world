8022d75b523b523476cbce14bc60a0ee6c6e4f33
Sumit Kumar Tiwary <sumitkrnitjsr@gmail.com>
2018-01-14 19:44:36 +0530

    Searching More Questions

---
+++ Array/Searching And Sorting/element_sorted_array_infinite_numbers.c
@@ -0,0 +1,40 @@
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r>=l)
+    {
+        int mid = l + (r - l)/2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid-1, x);
+        return binarySearch(arr, mid+1, r, x);
+    }
+    return -1;
+}
+
+// function takes an infinite size array and a key to be
+//  searched and returns its position if found else -1.
+// We don't know size of arr[] and we can assume size to be
+// infinite in this function.
+// NOTE THAT THIS FUNCTION ASSUMES arr[] TO BE OF INFINITE SIZE
+// THEREFORE, THERE IS NO INDEX OUT OF BOUND CHECKING
+int findPos(int arr[], int key)
+{
+    int l = 0, h = 1;
+    int val = arr[0];
+    
+    // Find h to do binary search
+    while (val < key)
+    {
+        l = h;        // store previous high
+        h = 2*h;      // double high index
+        val = arr[h]; // update new val
+    }
+    
+    // at this point we have updated low and
+    // high indices, Thus use binary search
+    // between them
+    return binarySearch(arr, l, h, key);
+}
